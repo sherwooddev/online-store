94c3fac965012b88cd66e7bc72a29498da587727
John Sherwood <jrsherwood@gmail.com>
2017-10-12 18:39:52 -0500

    trying again

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var bodyParser = require("body-parser");
-var test = "test":
 var app = express();
 app.use(bodyParser.json());
 
 // Create link to Angular build directory
 var distDir = __dirname + "/dist/";
 app.use(express.static(distDir));
+
+app.listen(process.env.PORT || 8080);
