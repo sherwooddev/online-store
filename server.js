3a7b54229c1cba4509cd9f8bdc652344c49b257d
John Sherwood <jrsherwood@gmail.com>
2017-10-12 18:06:16 -0500

    still working on server startup

--- server.js
+++ server.js
@@ -1,27 +1,9 @@
-// const compression = require('compression');
-// const path = require('path');
-const express = require('express');
-// const app = express();
-// const port = process.env.PORT || 4200;
+var express = require('express');
+var bodyParser = require("body-parser");
 
-// Gzip
-//app.use(compression());
+var app = express();
+app.use(bodyParser.json());
 
-// Run the app by serving the static files in the dist directory
-app.use(express.static(__dirname + '/dist'));
-
-// Start the app by listening on the default Heroku port
-// app.listen(port);
-
-// For all GET requests, send back index.html so that PathLocationStrategy can be used
-// app.get('/*', function(req, res) {
-//   res.sendFile(path.join(__dirname + '/dist/index.html'));
-// });
-
-// console.log(`Server listening on ${port}`);
-
-// Initialize the app.
-var server = app.listen(process.env.PORT || 8080, function () {
-  var port = server.address().port;
-  console.log("App now running on port", port);
-});
+// Create link to Angular build directory
+var distDir = __dirname + "/dist/";
+app.use(express.static(distDir));
