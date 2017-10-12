e93dae5c0479ed48525919a91a6f9f38de9533df
John Sherwood <jrsherwood@gmail.com>
2017-10-12 17:46:00 -0500

    add express static back to server file

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const express = require('express');
 //app.use(compression());
 
 // Run the app by serving the static files in the dist directory
-//app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist'));
 
 // Start the app by listening on the default Heroku port
 // app.listen(port);
