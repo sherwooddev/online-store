dfe7258460a59c61587226524fcff43f467da0e2
John Sherwood <jrsherwood@gmail.com>
2017-10-14 14:41:31 -0500

    Add album detail service

---
+++ src/app/album.service.spec.ts
@@ -0,0 +1,14 @@
+import { TestBed, inject } from '@angular/core/testing';
+import { AlbumService } from './album.service';
+
+describe('Album.Service', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [AlbumService]
+    });
+  });
+
+  it('should be created', inject([AlbumService], (service: AlbumService) => {
+    expect(service).toBeTruthy();
+  }));
+});
